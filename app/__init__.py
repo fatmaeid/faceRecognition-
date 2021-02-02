a8b4035c68ab6311a77b36acacf6d96877790dae
fatmaeid <eidf497@gmail.com>
2021-02-03 01:52:02 +0200

    views and init modification

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask 
 app = Flask(__name__)
 from app import models
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,24 +1,26 @@
+from flask import render_template
+
 @app.route('/')
-def GetInformation():
+def getInformation():
    return render_template('GetInformation.html')
 
 @app.route('/login')
 def login():
-   return render_template(login.html)
+   return render_template('login.html')
 
 @app.route('/getreport')
 def getreport():
-   return render_template(getreport.html)
+   return render_template('getreport.html')
 
 
 @app.route('/UpLoadImage')
-def UpLoadImage():
-   return render_template(UpLoadImage.html)    
+def upLoadImage():
+   return render_template('UpLoadImage.html')    
 
 @app.route('/reg')
 def reg():
-   return render_template(reg.html)
+   return render_template('reg.html')
 
 @app.route('/take_attendance')
 def take_attendance():
-   return render_template(take_attendance.html)
+   return render_template('take_attendance.html')
